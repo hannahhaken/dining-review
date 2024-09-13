aaea3711275da4d839581cbdca9846174102a9b5
Hannah Haken <hannah.haken@autotrader.co.uk>
2024-09-13 10:36:50 +0100

    initial commit – creates spring boot app and h2 database

---
+++ src/main/java/com/hannahhaken/dining_review/DiningReviewApplication.java
@@ -0,0 +1,13 @@
+package com.hannahhaken.dining_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hannahhaken/dining_review/DiningReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hannahhaken.dining_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
