a0e2cd74502519f326c423bd89e83373b8374ce9
Hannah Haken <hannah.haken@autotrader.co.uk>
2024-09-19 13:57:37 +0100

    Adds test for creating and retrieving users

--- src/main/java/com/hannahhaken/dining_review/repository/UserRepository.java
+++ src/main/java/com/hannahhaken/dining_review/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.hannahhaken.dining_review.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
 
 }
--- src/test/java/com/hannahhaken/dining_review/repository/UserRepositoryTest.java
+++ src/test/java/com/hannahhaken/dining_review/repository/UserRepositoryTest.java
@@ -1,4 +1,39 @@
 package com.hannahhaken.dining_review.repository;
 
+import com.hannahhaken.dining_review.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void can_create_and_retrieve_user() {
+
+        //given
+        User expectedUser = new User();
+        expectedUser.setUsername("Cloud Strife");
+        expectedUser.setCity("Manchester");
+        expectedUser.setCountry("England");
+
+        userRepository.save(expectedUser);
+
+        //when
+        User newUser = userRepository.findByUsername("Cloud Strife");
+
+        //then
+
+        assertThat("User ID should not be null", newUser.getId(), notNullValue());
+        assertEquals(expectedUser.getUsername(), newUser.getUsername());
+        assertEquals(expectedUser.getCity(), newUser.getCity());
+        assertEquals(expectedUser.getCountry(), newUser.getCountry());
+    }
 }
