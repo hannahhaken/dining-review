afa3880cc88c2c5d0d5c7c6d5fbe78c1fce0c750
Hannah Haken <hannah.haken@autotrader.co.uk>
2024-09-20 16:53:20 +0100

    Removes unused dependency and blank line

--- src/test/java/com/hannahhaken/dining_review/repository/UserRepositoryTest.java
+++ src/test/java/com/hannahhaken/dining_review/repository/UserRepositoryTest.java
@@ -13,7 +13,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @ActiveProfiles("test")
