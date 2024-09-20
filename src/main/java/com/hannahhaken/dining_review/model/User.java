2ee94c5d048dd9c2a9ce78c682297c5572b74711
Hannah Haken <hannah.haken@autotrader.co.uk>
2024-09-20 16:33:53 +0100

    Adds test for duplicate username scenario

--- src/main/java/com/hannahhaken/dining_review/model/User.java
+++ src/main/java/com/hannahhaken/dining_review/model/User.java
@@ -1,5 +1,6 @@
 package com.hannahhaken.dining_review.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -10,7 +11,10 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(unique = true)
     private String username;
+
     private String city;
     private String country;
 
--- src/test/java/com/hannahhaken/dining_review/repository/UserRepositoryTest.java
+++ src/test/java/com/hannahhaken/dining_review/repository/UserRepositoryTest.java
@@ -1,19 +1,29 @@
 package com.hannahhaken.dining_review.repository;
 
 import com.hannahhaken.dining_review.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Test
     public void can_create_and_retrieve_user() {
@@ -30,10 +40,31 @@ public class UserRepositoryTest {
         User newUser = userRepository.findByUsername("Cloud Strife");
 
         //then
-
         assertThat("User ID should not be null", newUser.getId(), notNullValue());
         assertEquals(expectedUser.getUsername(), newUser.getUsername());
         assertEquals(expectedUser.getCity(), newUser.getCity());
         assertEquals(expectedUser.getCountry(), newUser.getCountry());
+    }
+
+    @Test
+    public void should_throw_exception_when_saving_duplicate_username() {
+        String duplicateUsername = "Cloud Strife";
+        User firstUser = createUser(duplicateUsername);
+        userRepository.save(firstUser);
+        entityManager.flush();
+
+        User duplicateUser = createUser(duplicateUsername);
+
+        userRepository.save(duplicateUser);
+
+        assertThatThrownBy(() -> entityManager.flush())
+                .isInstanceOf(ConstraintViolationException.class)
+                .hasMessageContaining("could not execute statement");
+    }
+
+    private User createUser(String username) {
+        User user = new User();
+        user.setUsername(username);
+        return user;
     }
 }
